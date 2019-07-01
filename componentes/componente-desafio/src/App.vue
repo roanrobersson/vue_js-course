f33741bde269536be0c8298881923ca5e50c547c
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2019-07-01 10:02:35 -0300

    modulo 7 - capitulo 8 (Desafio concluido)

--- componentes/componente-desafio/src/App.vue
+++ componentes/componente-desafio/src/App.vue
@@ -1,33 +1,13 @@
 <template>
 	<v-app>
-		<v-navigation-drawer app>
-			<v-toolbar flat>
-				<v-list>
-					<v-list-tile>
-						<v-list-tile-title class="title">
-							Menu
-						</v-list-tile-title>
-					</v-list-tile>
-				</v-list>
-			</v-toolbar>
 
-			<v-divider></v-divider>
-
-			<v-list dense class="pt-0">
-				<v-list-tile v-for="item in itensMenu" :key="item.titulo">
-					<v-list-tile-action>
-						<v-icon>{{ item.icon }}</v-icon>
-					</v-list-tile-action>
-					<v-list-tile-content>
-						<v-list-tile-title>{{ item.titulo }}</v-list-tile-title>
-					</v-list-tile-content>
-				</v-list-tile>
-			</v-list>
-		</v-navigation-drawer>
+		<main-menu />
 
 		<toolbar />
 
-		<carousel />
+		<v-content>
+			<carousel />
+		</v-content>
 
 		<main-footer />
 		
@@ -37,21 +17,20 @@
 <script>
 import MainFooter from './components/MainFooter.vue';
 import Carousel from './components/Carousel.vue';
-import Toolbar from './components/Toolbar.vue';
+import Toolbar from './components/Toolbar.vue';;
+import MainMenu from './components/MainMenu.vue';
 
 export default {
 	data() {
 		return {
-			itensMenu: [
-				{titulo: 'Início', icon: 'dashboard'},
-				{titulo: 'Sobre', icon: 'question_answer'}
-			],
+
 		}
 	},
 	components: {
 		'main-footer': MainFooter,
 		'carousel': Carousel,
 		'toolbar': Toolbar,
+		'main-menu': MainMenu,
 	},
 }
 
