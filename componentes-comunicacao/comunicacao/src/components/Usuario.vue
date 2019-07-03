4abc551c10fdedddd0be989bb4071f00ff6dd974
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2019-07-03 00:07:02 -0300

    modulo 8 - capitulo 7

---
+++ componentes-comunicacao/comunicacao/src/components/Usuario.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container">
+        <h1>Componente Usuário</h1>
+        <p>Esse é um componente muito legal!</p>
+        <button @click='changeName()'>Alterar nome!</button>
+        <hr>
+        <div class="componentes">
+            <app-usuario-info :name='name'/>
+            <app-usuario-editar />
+        </div>
+    </div>
+</template>
+
+<script>
+import AppUsuarioInfo from './UsuarioInfo'
+import AppUsuarioEditar from './UsuarioEditar'
+
+export default {
+    components: { AppUsuarioInfo, AppUsuarioEditar },
+    data () {
+        return {
+            name : 'Suelen Moreira', 
+        }
+    },
+    methods: {
+        changeName: function() {
+            this.name = 'Nome alterado :)'
+        }
+    },
+}
+</script>
+
+<style scoped>
+    .container {
+        background-color: #27363b;
+        color: #fff;
+        padding: 10px;
+    }
+
+    .container hr {
+        margin: 20px 10px;
+    }
+
+    .componentes {
+        display: flex;
+    }
+
+    .componentes > * {
+        margin: 10px;
+    }
+</style>
