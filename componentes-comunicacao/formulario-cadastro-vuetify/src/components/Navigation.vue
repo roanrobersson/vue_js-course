93ec0b6b69191880d30f0443420e2c404b5ab9f5
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2019-07-17 01:11:18 -0300

    Add footer, navbar e toolbar

---
+++ componentes-comunicacao/formulario-cadastro-vuetify/src/components/Navigation.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-navigation-drawer permanent >
+        <v-list dense class="pt-0">
+        <v-list-tile
+            v-for="item in items"
+            :key="item.title"
+            @click="vaca"
+        >
+            <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+        </v-list-tile>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: null,
+            items: [
+                {title: 'Opção1'},
+                {title: 'Opção2'},
+                {title: 'Opção3'},
+                {title: 'Opção4'},
+                {title: 'Opção5'},
+            ]
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- componentes-comunicacao/formulario-cadastro-vuetify/src/views/Register.vue
+++ componentes-comunicacao/formulario-cadastro-vuetify/src/views/Register.vue
@@ -1,20 +1,44 @@
 <template>
+
     <v-app>
-        <v-container fluid fill-height>
-            <v-layout align-center justify-center row fill-height>
-                <v-flex xs12 sm8 md4>
-                    <form-register/>
-                </v-flex>
-            </v-layout> 
-        </v-container>
+
+        <app-toolbar />
+
+        <v-layout>
+
+            <app-navigation />
+
+            <v-container fluid>
+                <v-layout align-center justify-center row fill-height>
+                    <v-flex xs5 sm5 md5>
+                        <app-form-register />
+                    </v-flex>
+                </v-layout>
+            </v-container>
+
+        </v-layout>
+            
+        
+
+        <app-footer />
+
     </v-app>
+
 </template>
 
 <script>
+import Toolbar from '@/components/Toolbar.vue';
 import FormRegister from '@/components/FormRegister.vue';
+import Navigation from '@/components/Navigation.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
-  components: {'form-register': FormRegister},
+  components: {
+      'app-toolbar': Toolbar,
+      'app-navigation': Navigation,
+      'app-form-register': FormRegister,
+      'app-footer' : Footer,
+      },
   data() {
       return {
 
