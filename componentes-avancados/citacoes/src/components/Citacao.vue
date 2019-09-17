c598c2b50bd35fdde749c8d7305437c9702eef48
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2019-09-16 22:52:25 -0300

    modulo 9 - capitulo 3

---
+++ componentes-avancados/citacoes/src/components/Citacao.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="citacao">
+        {{ texto }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        texto: {
+            type: String,
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
