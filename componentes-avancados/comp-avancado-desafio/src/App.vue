ce3b13648fbfd5943b7915c3434aa32675d7072c
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2019-09-25 00:01:36 -0300

    modulo 10 - capitulo 1

---
+++ componentes-avancados/comp-avancado-desafio/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+	<div id="app">
+		<span>
+			<button class="vermelho" @click="componenteColorido = 'Vermelho'">Carregar Componente Vermelho</button>
+			<button class="verde" @click="componenteColorido = 'Verde'">Carregar Componente Verde</button>
+			<button class="azul" @click="componenteColorido = 'Azul'">Carregar Componente Azul</button>
+		</span>
+		
+        <component  :is="componenteColorido">
+            <span>
+                Conteúdo do Componente <strong>{{componenteColorido}}</strong>
+            </span>
+        </component> 
+	</div>
+</template>
+
+<script>
+import Vermelho from './components/Vermelho.vue'
+import Verde from './components/Verde.vue'
+import Azul from './components/Azul.vue'
+
+export default {
+	name: 'app',
+    components: { Vermelho, Verde, Azul },
+    data() {
+        return {
+            componenteColorido: null
+        }
+    }
+}
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+
+	button {
+		padding: 10px;
+		color: #FFF;
+		font-size: 1.3rem;
+	}
+
+    button:hover{
+        border: 2px solid white;
+    }
+
+    .caixa {
+        color: #FFF;
+        font-size: 1.8rem;
+        padding: 25px;
+		margin: 10px 0px;
+    }
+
+	.vermelho {
+        border: 2px solid red;
+        background-color: #f54235;
+    }
+
+	.verde {
+        border: 2px solid green;
+        background-color: #49b057;
+    }
+
+	.azul {
+        border: 2px solid blue;
+        background-color: #2594f0;
+    }
+</style>
