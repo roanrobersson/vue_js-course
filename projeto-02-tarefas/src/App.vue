5d72354748cdec2f76391750b700d18183b2bb64
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2019-09-27 01:19:39 -0300

    modulo 10 - capitulo 1 (HTML e CSS da tarefa 99% prontos, iniciando programação dos scripts)

---
+++ projeto-02-tarefas/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+	<div id="app">
+		<h1>Tarefas</h1>
+        <TaskProgress :progress="progress"/>
+        <NewTask @create-new-task="createNewTask($event)"/>
+        <TaskGrid />
+	</div>
+</template>
+
+<script>
+import TaskProgress from "@/components/TaskProgress.vue"
+import NewTask from "@/components/NewTask.vue"
+import TaskGrid from "@/components/TaskGrid"
+
+export default {
+    components: {TaskProgress, NewTask, TaskGrid},
+
+    data() {
+        return {
+            //Somente para teste...
+            total: 10,
+            done: 5,
+        }
+
+    },
+
+    computed: {
+        progress() { return Math.round( 100 / this.total * this.done) }
+    },
+
+    methods: {
+        createNewTask(e) { console.log(e); },
+    }
+}
+</script>
+
+<style>
+	body {
+		font-family: 'Lato', sans-serif;
+		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+		color: #FFF;
+	}
+
+	#app {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	#app h1 {
+		margin-bottom: 5px;
+		font-weight: 300;
+		font-size: 3rem;
+	}
+</style>
---
+++ projeto-02-tarefas/src/components/TaskGrid.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="task-grid">
+        <task />
+        <task />
+        <task />
+        <task />
+    </div>
+</template>
+
+<script>
+import Task from "@/components/Task.vue"
+
+export default {
+    components: {Task, },
+}
+</script>
+
+<style scoped>
+    .task-grid {
+        display: flex;
+        flex-wrap: wrap;
+        margin: 0 100px;
+    }
+</style>
