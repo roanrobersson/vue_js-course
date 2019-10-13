3151fa90530fa50cba46cc1774d0d44e2033efba
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2019-10-13 14:49:51 -0300

    modulo 14 - capitulo 22 (Iniciando Projeto)

---
+++ animacoes/animacoes-exercicios/src/AlertaAdvertencia.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-alert variant="warning" show>Uma advertência importante!</b-alert>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -2,27 +2,136 @@
 	<div id="app" class="container-fluid">
 		<h1>Animações</h1>
         <hr>
-        <b-button variant="primary" class="mb-4" 
+        <!-- <b-button variant="primary" class="mb-4" 
             @click="exibir = !exibir">Mostrar Mensagem</b-button>
         
-        <transition name="fade">
+        <transition name="fade" appear>
             <b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
         </transition>
+
+        <hr>
+
+        <transition name="slide" appear>
+            <b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+        </transition>
+
+        <hr>
+
+        <transition name="slide" 
+            enter-active-class="animated bounce"
+            leave-active-class="animated shake"
+            appear>
+            <b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+        </transition>
+
+
+        <hr>
+
+        <b-select v-model="tipoAnimacao">
+            <option value="fade">Fade</option>
+            <option value="slide">Slide</option>
+        </b-select>
+
+        <hr>
+
+        <transition :name="tipoAnimacao" appear mode="out-in">
+            <b-alert variant="info" show v-if="exibir" key="info">{{ msg }}</b-alert>
+            <b-alert variant="warning" show v-else key="warning">{{ msg }}</b-alert>
+        </transition>
+
+        <hr>
+
+        <button @click="exibir2 = !exibir2">Alternar</button>
+
+        <transition
+            :css="false"
+            @before-enter="beforeEnter"
+            @enter="enter"
+
+            @before-leave="beforeLeave"
+            @leave="leave">
+            <div v-if="exibir2" class="caixa"></div>
+        </transition>
+
+        <hr>
+        <div class="mb-4">
+            <b-button variant="primary" class="mr-2"
+                @click="componenteSelecionado = 'AlertaInfo'">Info</b-button>
+            <b-button variant="secondary" 
+                @click="componenteSelecionado = 'AlertaAdvertencia'">Advertência</b-button>
+        </div>
+       
+       <transition name="fade" mode="out-in">
+            <component :is="componenteSelecionado"></component>
+        </transition> -->
+
+        <hr>
+
+        <b-button @click="adicionarAluno" class="mb-4">Adicionar Aluno</b-button>
         
-        <transition name="slide">
-            <b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
-        </transition>
-	</div>
+        <transition-group name="slide" tag="div">
+            <b-list-group v-for="(aluno, i) in alunos" :key="aluno">
+                <b-list-group-item @click="removerAluno(i)">{{ aluno }}</b-list-group-item>
+            </b-list-group>
+        </transition-group>
+    </div>
 </template>
 
 <script>
+import AlertaAdvertencia from './AlertaAdvertencia.vue'
+import AlertaInfo from './AlertaInfo.vue'
 
 export default {
+    components: { AlertaAdvertencia, AlertaInfo },
     data() {
         return {
+            alunos: ['Roberto', 'Julia', 'Teresa', 'Paulo'],
             msg: 'Uma mensagem de informação para usuário',
-            exibir: false,
+            exibir: true,
+            exibir2: true,
+            tipoAnimacao: 'fade',
+            larguraBase: 0,
+            componenteSelecionado: 'AlertaInfo'
         }
+    },
+    methods: {
+        adicionarAluno() {
+            const s = Math.random().toString(36).substring(2)
+            this.alunos.push(s)
+        },
+        removerAluno(indice) {
+            this.alunos.splice(indice, 1)
+        },
+        animar(el, done, negativo) {
+            let rodada = 1
+            const temporizador = setInterval(() => {
+                const novaLargura = this.larguraBase + 
+                    (negativo ? -rodada * 10 : rodada * 10)
+                el.style.width = novaLargura + 'px'
+                rodada++
+                if (rodada > 30) {
+                    clearInterval(temporizador)
+                    done()
+                }
+            }, 20)
+        },
+        beforeEnter(el) {
+            this.larguraBase = 0
+            el.style.width = this.larguraBase + 'px'
+        },
+        enter(el, done) {
+            this.animar(el, done, false)
+        },
+
+        /////////////////////////////////////
+        beforeLeave(el) {
+            this.larguraBase = 300
+            el.style.width = this.larguraBase + 'px'
+        },
+        leave(el, done) {
+            this.animar(el, done, true)
+        },
+
     }
 }
 </script>
@@ -38,41 +147,56 @@ export default {
 	font-size: 1.5rem;
 }
 
-/* Enter */
-.fade-enter {
-    opacity: 0;
+.caixa {
+    height: 100px;
+    width: 300px;
+    margin: 30px auto;
+    background-color: lightgreen; 
 }
+
+/*-----------------*/
+/*       FADE      */
+/*-----------------*/
+.fade-enter {}
 
 .fade-enter-active {
-    transition: opacity 0.5s;
+    transition: opacity 1s;
 }
 
-.fade-enter-to {
-    opacity: 1;
-}
+.fade-enter-to {}
 
-/* Leave */
-.fade-leave {
-    opacity: 1;
-}
+.fade-leave {}
 
 .fade-leave-active {
     transition: opacity 1s;
 }
 
-.fade-leave-to {
-    opacity: 0;
-}
+.fade-leave-to {}
 
 
+/*-----------------*/
+/*      SLIDE      */
+/*-----------------*/
 @keyframes slide-in {
-    from { transform: translateY(40px); }
-    to { transform: translateY(0); }
+    from { 
+        transform: translateY(40px); 
+        opacity: 0;
+    }
+    to { 
+        transform: translateY(0); 
+        opacity: 1;
+    }
 }
 
 @keyframes slide-out {
-    from { transform: translateY(0); }
-    to { transform: translateY(40px); }
+    from { 
+        transform: translateY(0); 
+        opacity: 1;
+    }
+    to { 
+        transform: translateY(40px); 
+        opacity: 0;
+    }
 }
 
 .slide-enter-active {
@@ -80,7 +204,14 @@ export default {
 }
 
 .slide-leave-active {
+    position: absolute;
+    width: 100%;
     animation: slide-out 1s ease;
+}
+
+
+.slide-move {
+    transition: transform 1s;
 }
 
 </style>
